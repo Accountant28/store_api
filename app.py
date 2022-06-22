6ba3571f13112a9e84e5ad7608da3d668a74f23b
Vladislav <carlsberg-28@yandex.ru>
2022-06-22 12:59:57 +0300

    Fixex main.py with postgres

--- app.py
+++ app.py
@@ -12,7 +12,17 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+print(uri)
+
+if uri == None:
+    uri = "postgresql://"
+else:
+    # uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
@@ -28,6 +38,7 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
 
     if app.config['DEBUG']:
